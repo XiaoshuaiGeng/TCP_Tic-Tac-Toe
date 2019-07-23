c915d0ebdf892991e1de93621f863b32eb7e51d6
Xiaoshuai Geng <genglittlehandsome@hotmail.com>
2019-07-23 12:52:03 -0400

    Add files via upload

---
+++ Client/TicTacToe.java
@@ -0,0 +1,27 @@
+    
+
+public class TicTacToe {
+
+	public static void main(String[] args) {
+		//System.out.println("hello");
+		GUI_board board = new GUI_board();
+		//System.out.println(board.start);
+		board.lockBoard();
+		while(true) {
+			System.out.print("");
+			System.out.flush();
+			if(board.start) {
+				board.client.run();
+				//board.unlockBoard();
+			}
+				
+			board.start=false;
+			if(board.exitFlag == true)
+				break;
+		}
+		//board.getClient().run();
+		//()
+		
+	}
+
+}
